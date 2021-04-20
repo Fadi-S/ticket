dfdef28f02c8ef75e18910a248acea35ad52847d
Fadi Sarwat <fady.sarwat377@gmail.com>
2021-04-20 13:54:48 +0200

    Changed server

--- app/Http/Middleware/SecureConnection.php
+++ app/Http/Middleware/SecureConnection.php
@@ -15,14 +15,14 @@ class SecureConnection
      */
     public function handle($request, Closure $next)
     {
-        if(app()->environment() == "production") {
+        /*if(app()->environment() == "production") {
             if (!$request->secure() || !str_starts_with($request->header('host'), 'ticket.')) {
                 if (!str_starts_with($request->header('host'), 'ticket.'))
                     $request->headers->set('host', 'ticket.' . $request->header('host'));
 
                 return \Redirect::to(str_replace("http://", 'https://', $request->fullUrl()), 302);
             }
-        }
+        }*/
 
         /*if ($request->secure() || !str_starts_with($request->header('host'), 'kat.')) {
             if(!str_starts_with($request->header('host'), 'service.'))
