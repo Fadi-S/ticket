3e29df8e166453dfa70cf42e73c87c5175fc8749
Fadi Sarwat <fady.sarwat377@gmail.com>
2021-04-29 03:52:13 +0200

    Change default type for cancel button to button

--- resources/views/components/buttons/cancel.blade.php
+++ resources/views/components/buttons/cancel.blade.php
@@ -2,6 +2,7 @@
                                 border border-red-600 hover:bg-red-100 text-red-500
                                 dark:focus:text-red-200 dark:hover:text-red-200
                                     focus:bg-red-100 dark:focus:bg-red-500
-                                     dark:hover:bg-red-500 transition-dark">
+                                     dark:hover:bg-red-500 transition-dark"
+        type="button">
     <x-svg.x size="h-5 h-5" />
 </button>
