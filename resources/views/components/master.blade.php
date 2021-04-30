b6d1eb05b99ec526891f12cbf42f80f294671222
Fadi Sarwat <fady.sarwat377@gmail.com>
2021-04-30 16:37:23 +0200

    Remove test

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -62,8 +62,6 @@
 
                 <x-navbar.divider/>
 
-                Hellooooo
-
                 <x-navbar.link label="{{ __('Make Reservation') }}" data-turbolinks="false" :href="url('/reserve')">
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                          xmlns="http://www.w3.org/2000/svg">
