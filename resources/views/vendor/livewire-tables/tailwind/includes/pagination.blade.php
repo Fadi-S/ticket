1fcd98cae0b508cf8d6d592ed89eea071645e055
Fadi Sarwat <fady.sarwat377@gmail.com>
2021-05-06 23:54:43 +0200

    Fix pagination number not showing

--- resources/views/vendor/livewire-tables/tailwind/includes/pagination.blade.php
+++ resources/views/vendor/livewire-tables/tailwind/includes/pagination.blade.php
@@ -3,7 +3,7 @@
         @if ($paginationEnabled && $showPerPage && $rows->lastPage() > 1)
             {{ $rows->links() }}
         @else
-            <p class="text-sm text-gray-700 leading-5">
+            <p class="text-sm text-gray-700 dark:text-gray-100 leading-5">
                 @lang('Showing')
                 <span class="font-medium">{{ $rows->count() }}</span>
                 @lang('results')
