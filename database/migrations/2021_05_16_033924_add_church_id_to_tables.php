8c8e438dc6cd3dff9085c996789f336f16755b41
Fadi Sarwat <fady.sarwat377@gmail.com>
2021-05-17 21:15:12 +0200

    Add migrations, middleware and scope to limit each user to their church

---
+++ database/migrations/2021_05_16_033924_add_church_id_to_tables.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddChurchIdToTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $addColumn = fn (Blueprint $table) =>
+            $table->foreignId('church_id')->constrained('churches')->nullable();
+
+
+        Schema::table('users', $addColumn);
+
+        Schema::table('events', $addColumn);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $dropColumn = fn (Blueprint $table) => $table->dropColumn('church_id');
+
+        Schema::table('users', $dropColumn);
+
+        Schema::table('events', $dropColumn);
+    }
+}
