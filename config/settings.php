68adcaca405f191462f1debd83105d959d5b0c03
Fadi Sarwat <fady.sarwat377@gmail.com>
2021-05-22 13:30:33 +0200

    Change login image

--- config/settings.php
+++ config/settings.php
@@ -16,8 +16,8 @@ return [
 
     'google_api_token' => env('GOOGLE_API_TOKEN', null),
 
-    'light_theme_photo' => env('LIGHT_THEME_PHOTO', '/images/stmary_jesus-500-min.jpg'),
-    'dark_theme_photo' => env('DARK_THEME_PHOTO', '/images/stmary_jesus-500-min.jpg'),
+    'light_theme_photo' => env('LIGHT_THEME_PHOTO', '/images/saintmarc.jpg'),
+    'dark_theme_photo' => env('DARK_THEME_PHOTO', '/images/saintmarc.jpg'),
 
     'users_limit' => 10,
 
