4f4760626c535a84b7d7b3f7db062101eda86f62
Fadi Sarwat <fady.sarwat377@gmail.com>
2021-05-24 01:45:07 +0200

    Add Open Mass

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -137,6 +137,23 @@
 
                     </x-navbar.list>
 
+                    <x-navbar.list label="{{ __('Open Masses') }}">
+
+                        <x-slot name="svg">
+                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path>
+                            </svg>
+                        </x-slot>
+
+                        @can("events.create")
+                            <x-navbar.child label="{{ __('Add Mass') }}" href="{{ url('/masses-open/create') }}"/>
+                        @endcan
+
+                        <x-navbar.child label="{{ __('View Masses') }}" href="{{ url('/masses-open') }}"/>
+
+                    </x-navbar.list>
+
                     <x-navbar.list label="{{ __('Vespers') }}">
 
                         <x-slot name="svg">
