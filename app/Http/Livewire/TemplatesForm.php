a25651a691a681d30ec201dae314bee6cc49f304
Fadi Sarwat <fady.sarwat377@gmail.com>
2021-05-25 13:21:06 +0200

    Fix web routes (#54)

--- app/Http/Livewire/TemplatesForm.php
+++ app/Http/Livewire/TemplatesForm.php
@@ -68,7 +68,8 @@ class TemplatesForm extends Component
 
         $this->template->start = $this->start;
         $this->template->end = $this->end;
-        $this->template->type_id = $this->type_id ?? Mass::$type;
+        $this->template->type_id = $this->type_id ?? 1;
+        $this->template->church_id = auth()->user()->church_id;
         $this->template->active ??= true;
         $this->template->overload = $this->template->overload / 100;
         $this->template->save();
