4c63b1d8a80ea176038ad40536da95507a7fa573
Fadi Sarwat <fady.sarwat377@gmail.com>
2021-05-28 12:46:52 +0200

    Update User.php

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -30,7 +30,7 @@ class User extends Authenticatable implements HasLocalePreference
 
     protected $hidden = ['password', 'remember_token'];
     protected $casts = ['email_verified_at' => 'datetime', 'gender' => 'boolean'];
-    protected $fillable = ['name', 'arabic_name', 'email', 'password', 'username', 'picture', 'gender', 'phone', 'national_id', 'creator_id', 'activated_at'];
+    protected $fillable = ['name', 'arabic_name', 'email', 'password', 'username', 'picture', 'gender', 'phone', 'national_id', 'creator_id', 'activated_at', 'location_id'];
     protected $dates = [
         'activated_at'
     ];
