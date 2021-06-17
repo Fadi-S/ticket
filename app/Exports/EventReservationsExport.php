6cff0cd22ba154245ecb57d0f04548face737589
Fadi Sarwat <fady.sarwat377@gmail.com>
2021-06-17 21:43:08 +0200

    Changes in excel

--- app/Exports/EventReservationsExport.php
+++ app/Exports/EventReservationsExport.php
@@ -69,15 +69,16 @@ class EventReservationsExport implements FromView, WithStyles, ShouldAutoSize
 
     public function styles(Worksheet $sheet)
     {
-        $sheet->getStyle('1')
-            ->getFont()
-            ->setBold(true);
-
         $sheet->getStyle('A:C')
             ->getAlignment()
             ->setHorizontal('center');
 
         $sheet->getStyle('A:C')
             ->getFont()->setSize(16);
+
+        $sheet->getStyle('1')
+            ->getFont()
+            ->setSize(14)
+            ->setBold(true);
     }
 }
