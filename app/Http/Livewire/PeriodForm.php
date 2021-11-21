b97927c158c75dffff4e40b2996286c934c2bb5a
Fadi Sarwat <fadisarwat@yahoo.com>
2021-11-21 23:05:18 +0200

    Add default type

--- app/Http/Livewire/PeriodForm.php
+++ app/Http/Livewire/PeriodForm.php
@@ -35,7 +35,7 @@ class PeriodForm extends Component
     public function render()
     {
         return view('livewire.period-form', [
-            'types' => EventType::shown()->pluck('arabic_name', 'id')->toArray()
+            'types' => [0 => '-'] + EventType::shown()->pluck('arabic_name', 'id')->toArray()
         ])->layout('components.master');
     }
 
